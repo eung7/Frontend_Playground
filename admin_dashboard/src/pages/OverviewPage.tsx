55d9dc383c6971a4e1644c4bf082b83ceb9789af
Eunzo <wannabeeung@gmail.com>
2025-05-25 22:10:11 +0900

    ✨ Add Sidebar

---
+++ admin_dashboard/src/pages/OverviewPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/common/Header";
+
+function OverviewPage() {
+  return (
+    <div className="w-full z-10">
+      <Header title="Overview" />
+    </div>
+  );
+}
+
+export default OverviewPage;
